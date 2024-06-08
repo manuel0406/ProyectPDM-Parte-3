0861acb214b370f1956179812b781275146983b0
UesMR19082 <mr19082@ues.edu.sv>
2024-06-08 00:33:21 -0600

    Correciones y ajustes de Pantallas de Pago

--- G05/app/src/main/java/sv/edu/ues/fia/telollevoya/pago/PagoAprobadoRActivity.java
+++ G05/app/src/main/java/sv/edu/ues/fia/telollevoya/pago/PagoAprobadoRActivity.java
@@ -21,6 +21,7 @@ import sv.edu.ues.fia.telollevoya.R;
 import sv.edu.ues.fia.telollevoya.Reservacion;
 import sv.edu.ues.fia.telollevoya.Reservaciones.DetallePedidoR;
 import sv.edu.ues.fia.telollevoya.pedidos.cliente.MisPedidosActivity;
+import sv.edu.ues.fia.telollevoya.pedidos.negocio.NegociosActivity;
 
 public class PagoAprobadoRActivity extends AppCompatActivity implements ControladorSevicio.ReservacionInsertListener {
     Button btnVerResumen;
@@ -34,93 +35,31 @@ public class PagoAprobadoRActivity extends AppCompatActivity implements Controla
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pago_aprobado);
+        setContentView(R.layout.activity_reservacion_aprobada);
 
-        Locale currentLocale = getResources().getConfiguration().locale;
         txAgradecimiento = findViewById(R.id.txtAgradecimiento);
         btnVerResumen = findViewById(R.id.btnVerResumen);
-
-        if (currentLocale.getLanguage().equals("en")) {
-            txAgradecimiento.setText("Payment approved! Your reservation has been confirmed. Thank you for choosing us!");
-            btnVerResumen.setText("View Reservation Summary");
-        } else {
-            txAgradecimiento.setText("¡Pago aprobado! Tu reservación ha sido confirmada. ¡Gracias por elegirnos!");
-            btnVerResumen.setText("Ver Resumen Reservación");
-        }
 
         reservacion = (Reservacion) getIntent().getSerializableExtra("reservacion");
         detallesPedidoR = getIntent().getParcelableArrayListExtra("listaDetalle");
 
         insertarReservacion();
 
-
-
-
-
     }
 
     //Metodos para los botones
-    public void solicitarVerFactura(View view) {
+    public void verResumenDeReservacion(View view) {
         Intent intent = new Intent(this, FacturaReservacionActivity.class);
         intent.putExtra("reservacion", reservacion);
         intent.putParcelableArrayListExtra("listaDetalle",  detallesPedidoR);
         startActivity(intent);
     }
 
-    public void verEstadoPedido(View view) {
-        Intent intent = new Intent(this, MisPedidosActivity.class);
+    public void regresarAListaDeNegocios(View view) {
+        Intent intent = new Intent(this, NegociosActivity.class);
         startActivity(intent);
+        finish();
     }
-
-//    public void insertarReservacion(){
-//
-//        String fechaEntregar="";
-//        SimpleDateFormat dateFormatEntrada = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
-//        SimpleDateFormat dateFormatSalida = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
-//        try {
-//            java.util.Date fecha = dateFormatEntrada.parse(reservacion.getFechaEntregaR());
-//            fechaEntregar = dateFormatSalida.format(fecha);
-//
-//        } catch (ParseException e) {
-//            e.printStackTrace();
-//            // Manejar el error de análisis de fecha aquí
-//        }
-//
-//    //aca empiezo a tratar los datos para no terner problemas con espacios y luego se insertan
-//                String idClienteCodificado = "",descripcionCodificado="",anticipoCodificado="",montoPendienteCodificado="",
-//                        fechaEntregaCodificado="",horaEntegaCodificado="",totalReservacionCodificado="";
-//            try {
-//                idClienteCodificado= URLEncoder.encode(String.valueOf(reservacion.getIdCliente()), "UTF-8");
-//                descripcionCodificado = URLEncoder.encode(reservacion.getDescripcionReservacion(), "UTF-8");
-//                anticipoCodificado=URLEncoder.encode(String.valueOf(reservacion.getAnticipoReservacion()), "UTF-8");
-//                montoPendienteCodificado=URLEncoder.encode(String.valueOf(reservacion.getMontoPediente()), "UTF-8");
-//                fechaEntregaCodificado= fechaEntregar;
-//                horaEntegaCodificado=URLEncoder.encode(reservacion.getHoraEntrega(), "UTF-8");
-//                totalReservacionCodificado= URLEncoder.encode(String.valueOf(reservacion.getTotalRerservacion()), "UTF-8");
-//
-//            } catch (UnsupportedEncodingException e) {
-//                e.printStackTrace();
-//                //return;
-//                //return "Error al codificar el nombre del departamento";
-//            }
-//
-//            String url = urlReservacion +"?IDCLIENTE="+idClienteCodificado+ "&DESCRIPCIONRESERVACION=" +
-//                    descripcionCodificado+"&ANTICIPORESERVACION="+anticipoCodificado+"&MONTOPENDIENTE="+
-//                    montoPendienteCodificado+"&FECHAENTREGAR="+fechaEntregaCodificado+"&HORAENTREGAR="+horaEntegaCodificado+
-//                    "&TOTALRESERVACION="+totalReservacionCodificado;
-//
-//            Log.v("URL completa", url); // Añadir esta línea para verificar la URL completa
-//            ControladorSevicio.insertarReservacion(url, this, this);
-//
-////        ControladorSevicio.insertarReservacion(urlReservacion, this, new ControladorSevicio.ReservacionInsertListener() {
-////            @Override
-////            public void onReservacionInserted(int idReservacion) {
-////                reservacion.setIdReservacion(idReservacion);
-////                Log.v("Se ingresó reservación con id: ", String.valueOf(idReservacion));
-////                //insertarDetallePedido(idReservacion);
-////            }
-////        });
-//    }
 
     public void insertarReservacion() {
         String fechaEntregar = "";
